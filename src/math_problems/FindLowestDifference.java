f06b0105e219fba332f3f37f0b741344510963d4
Viktorianinarb <viktorianinarb@gmail.com>
2022-10-15 23:57:22 -0400

    Math. Find Lowest Difference.

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,11 +1,18 @@
 package math_problems;
 
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindLowestDifference {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to return the lowest number, that is not shared between the 2 arrays below
      * HINT: The lowest number that isn't shared between these arrays is 1
+     *
+     * @return
      */
 
     public static void main(String[] args) {
@@ -14,8 +21,16 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
+        //remove items that are shared and find minimum
+
+        Set<Integer> nonShared = new HashSet<Integer>();
+        for (int a : array1) {
+            nonShared.add(a);
+        }
+        for (int a : array2) {
+            nonShared.remove(a);
+        }
+        int result = Collections.min(nonShared);
+        System.out.println(result);
     }
-
-
-
 }
